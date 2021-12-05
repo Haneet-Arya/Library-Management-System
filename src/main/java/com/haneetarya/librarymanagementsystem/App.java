d863a9b246154d10de3f74e30802e84a48f68549
Haneet-Arya <haneetarya16@gmail.com>
2021-12-05 18:09:30 +0530

    Documentation added

--- src/main/java/com/haneetarya/librarymanagementsystem/App.java
+++ src/main/java/com/haneetarya/librarymanagementsystem/App.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Library Management Tool");
         stage.setScene(scene);
